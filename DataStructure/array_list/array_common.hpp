367084d4b8262459d540035762f8b7d02df68679
bing <libingluohe@126.com>
2020-02-09 00:25:32 +0800

    添加insert方法

--- DataStructure/array_list/array_common.hpp
+++ DataStructure/array_list/array_common.hpp
@@ -11,39 +11,50 @@ template<typename T>
 class ArrayList{
     public:
         ArrayList();
-        // ~ArrayList();
+        ~ArrayList();
         int add(T t);
         T getByIndex(int index);
-        // int insertByIndex(int index);
+        int insertByIndex(T t, int index);
         // int modifyByIndex(int index, T const value);
         // T search();
+        int reSize(int size = 10);
     private:
         T *tPtr;
         unsigned int size;
         unsigned int used_size = 0;
-        unsigned int default_size = 10;
+        unsigned int default_capacity = 10;
 };
 
 template<typename T>
 ArrayList<T>::ArrayList(){
-    this->tPtr = new T[default_size];
-    size = default_size;
+    this->tPtr = new T[default_capacity];
+    size = default_capacity;
+}
+
+template<typename T>
+ArrayList<T>::~ArrayList(){
+    if(this->tPtr){
+        delete[] this->tPtr;
+        tPtr = NULL;
+    }
+}
+
+template<typename T>
+int ArrayList<T>::reSize(int resize){
+    T* new_tPtr = new T[this->size + resize];
+    memcpy(new_tPtr, tPtr, sizeof(T) * size);
+    size += resize;
+    T* old_tPtr = this->tPtr;
+    this->tPtr = new_tPtr;
+    delete[] old_tPtr;
+    old_tPtr = NULL;
+    return 0;
 }
 
 template<typename T>
 int ArrayList<T>::add(T t){
     if(used_size >= size){
-       T* new_tPtr = new T[size + default_size];
-       if(NULL == new_tPtr){
-           return -1;
-       }
-       memcpy(new_tPtr, tPtr, sizeof(T) * size);
-       new_tPtr[size] = t;
-       size += default_size;
-       T* old_tPtr = this->tPtr;
-       this->tPtr = new_tPtr;
-       delete[] old_tPtr;
-       old_tPtr = NULL;
+        this->reSize(default_capacity)；
     }else{
         tPtr[used_size] = t;
     }
@@ -53,10 +64,20 @@ int ArrayList<T>::add(T t){
 
 template<typename T>
 T ArrayList<T>::getByIndex(int index){
-    // if(index <0 || index > used_size){
-    //     throw exception ;
-    // }
     return tPtr[index];
 }
 
+template<typename T>
+int ArrayList<T>::insertByIndex(T t, int index){
+    T* new_tPtr = new T[this->size + default_capacity];
+    memcpy(new_tPtr, tPtr, sizeof(T) * index);
+    memcpy(&new_tPtr[index+1], &tPtr[index], sizeof(T) * (size-index));
+    new_tPtr[index] = t;
+    T* old_tPtr = this->tPtr;
+    this->tPtr = new_tPtr;
+    delete[] old_tPtr;
+    old_tPtr = NULL;
+    this->size += default_capacity;
+    ++this->used_size
+}
 #endif
