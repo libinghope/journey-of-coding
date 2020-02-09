3d1b187446a2d31d8cc830c0b68c116b87667ef4
bing <libingluohe@126.com>
2020-02-09 23:37:10 +0800

    c基础开始

--- DataStructure/array_list/array_list.cpp
+++ DataStructure/array_list/array_list.cpp
@@ -10,6 +10,15 @@ int main(int arg, char* args[]){
 	for (int i = 0; i < 10; i++) {
 		cout << arr.getByIndex(i) << endl;
 	}
+	ArrayList<string> strArr;
+	strArr.add("aaa");
+	strArr.add("bbb");
+	strArr.add("ccc");
+	strArr.add("444");
+	for(int i=0; i< 4; i++){
+		cout << strArr.getByIndex(i) << endl;
+	}
+
     system("pause");
     return 0;
 }
