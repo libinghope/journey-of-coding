473acc41771bca76e7f7d995086f21e6f5f0b9b6
[bing] <[libingjy@gmail.com]>
2020-02-10 21:56:47 +0800

    更新

--- c基础/003c语言嵌入汇编语言.c
+++ c基础/003c语言嵌入汇编语言.c
@@ -8,6 +8,11 @@
 2）编译：检查语法，将预处理后文件编译生成汇编文件
 3）汇编：将汇编文件生成目标文件(二进制文件)
 4）链接：C语言写的程序是需要依赖各种库的，所以编译之后还需要把库链接到最终的可执行程序中去
+gcc的处理流程如下:
+    预处理：gcc -E hello.c -o hello.i
+    编  译：gcc -S hello.i -o hello.s
+    汇  编：gcc -c hello.s -o hello.o
+    链  接：gcc    hello.o -o hello_elf
  */
 int main(){
     // 定义三个整数
