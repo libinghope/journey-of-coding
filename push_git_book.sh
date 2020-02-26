b4638802dc9bb594b52d689c729dab838b93d90a
libing <libingjy@gmail.com>
2020-02-26 19:49:57 +0800

    fix sh

--- push_git_book.sh
+++ push_git_book.sh
@@ -4,13 +4,16 @@ then
     echo "git commit message can't be empty!"
     exit 1
 fi
-cd /Users/bing/code/journey-of-coding
+cd /Users/bing/code/journey-of-coding/blog
+echo "gitbook building..."
+gitbook build
 echo "正在复制_book文件······"
+cd ..
 \cp -R ./blog/_book/* ../_book
 echo "复制_book完成"
 cd ../_book
 #提交到github
-git add *
+git add -A
 git commit -m "$message"
 echo "push to github······"
 git push origin gh-pages
