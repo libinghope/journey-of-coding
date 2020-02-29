947f71594ca4b922c19460ad0f3d4625d68bd9ab
libing <libingjy@gmail.com>
2020-03-01 01:21:29 +0800

    add donate function

--- push_hexo_blog.sh
+++ push_hexo_blog.sh
@@ -1,24 +1,26 @@
 message=$1
 if [ -z "$message" ]
 then
+!commit message 不能为空
     echo "git commit message can't be empty!exit..."
     exit 1
 fi
 #提交主分支
-echo "开始提交主分支master......"
+echo "git push origin master......"
 git add -A
 git commit -m "$message"
 git push origin master
-echo "主分支master提交完成......."
+echo "push master branch success."
 
 cd hexo-blog
-echo "clean cache......"
+echo "cleaning cache......"
 hexo clean
 echo "clean task is over......"
 echo "hexo-blog building......"
 hexo g
-
-#发布网站
+echo "hexo-blog has builded successfully."
+:<<!  
+hexo自带的hexo-deployer-git还要申请TOKEN,蠢得要死,不用了
 echo "hexo cleaning......"
 Run hexo clean 
 echo "hexo clean finished......"
@@ -27,19 +29,19 @@ echo "hexo deploying......"
 hexo deploy
 echo "hexo deploy finished......"
 echo "everything is ok......"
+!
 
-:<<!
-echo "正在复制web文件······"
+echo "coping web static file······"
 cd ..
 rm -r ../blog/*
 \cp -R ./hexo-blog/public/* ../blog
-echo "复制_book完成"
+echo "finished copying static file"
 cd ../blog
 
-echo "提交gh-pages分支......"
+echo "git push origin gh-pages......"
 git add -A
 git commit -m "$message"
 git push origin gh-pages
 
-echo "push origin->gh-pages success."
-！
+echo "publish blog success. Go and to see your website."
+
