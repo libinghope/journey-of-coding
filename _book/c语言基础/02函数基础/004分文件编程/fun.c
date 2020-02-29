13c8d607d2bf2f19c6be3454755e7d59d85d1be4
libing <libingjy@gmail.com>
2020-02-29 13:36:58 +0800

    donate

--- _book/c语言基础/02函数基础/004分文件编程/fun.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include "fun.h"
-
-int max(int a, int b){
-    return a > b ? a:b;
-}
-
-
--- _book/c语言基础/02函数基础/004分文件编程/main.c
+++
@@ -1,9 +0,0 @@
-#include<stdlib.h>
-#include "fun.h"
-
-int main(){
-	printf("比较大的数是: %d..\n", max(99, 9));
-	system("pause");
- 
-	return 0;
-}
