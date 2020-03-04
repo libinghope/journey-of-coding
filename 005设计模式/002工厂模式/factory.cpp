f116865198cab3b6b03fcf213800aa6dca33c9a1
libing <libingjy@gmail.com>
2020-03-04 09:02:36 +0800

    简单工厂模式完成

--- 005设计模式/002工厂模式/factory.cpp
+++ 005设计模式/002工厂模式/factory.cpp
@@ -2,21 +2,34 @@
 using namespace std;
 
 //参考书籍，《大话设计模式》，《设计模式那些事》
-class BaseOperation{
+class Operation{
     public:
-    virtual double calculate();
-    double number1;
-    double number2;
+    virtual double calculate(double a, double b){}
 };
-class AddOperation :public BaseOperation{
-    virtual double calculate(){
+class AddOperation :public Operation{
+    virtual double calculate(double number1, double number2){
+        return number1-number2;
+    }
+};
+class SubOperation :public Operation{
+    virtual double calculate(double number1, double number2){
         return number1+number2;
+    }
+};
+class MulOperation :public Operation{
+    virtual double calculate(double number1, double number2){
+        return number1*number2;
+    }
+};
+class DivOperation :public Operation{
+    virtual double calculate(double number1, double number2){
+        return number1/number2;
     }
 };
 class OperationFactory
 {
 public:
-    static BaseOperation operationCreate(char oper)
+    static Operation* operationCreate(char oper)
     {
         switch (oper)
         {
@@ -24,19 +37,29 @@ public:
             return new AddOperation;
             break;
         case '-':
+            return new SubOperation;
             break;
         case '*':
+            return new MulOperation;
             break;
         case '/':
+            return new DivOperation;
             break;
 
         default:
-            break;
+            return NULL;
         }
     }
 };
 int main()
 {
+    double pi = 3.14;
+    double r  = 3.5;
+
+    Operation *operate = OperationFactory::operationCreate('*');
+
+    cout<< "pi * r = "<<operate->calculate(pi,r)<<endl;
+
 
     return 0;
 }
