d222ea593fb4a7f9b9db625fc54a763a4255ce8e
libing <libingjy@gmail.com>
2020-05-17 23:15:41 +0800

    grpc

---
+++ 018从0开始学微服务(已迁移至blog)/hello-grpc/client/gRPC-Client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	pt "client/mygrpc"
+	"context"
+	"fmt"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	post = "127.0.0.1:8000"
+)
+
+func main() {
+	conn, err := grpc.Dial(post, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("连接服务器失败", err)
+	}
+
+	defer conn.Close()
+
+	c := pt.NewHelloWorldServiceClient(conn)
+
+	//远程调用
+	r1, err := c.SayHello(context.Background(), &pt.HelloRequest{Name: "熊猫阿宝"})
+
+	if err != nil {
+		fmt.Println("can't get server", err)
+	}
+	fmt.Println(r1.Message)
+
+}
---
+++ 018从0开始学微服务(已迁移至blog)/hello-grpc/server/gRPC-Server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	pt "server/mygrpc"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	post = "127.0.0.1:8000"
+)
+
+type server struct{}
+
+//实现SayHello接口
+func (s *server) SayHello(ctx context.Context, in *pt.HelloRequest) (*pt.HelloReply, error) {
+	return &pt.HelloReply{Message: "hello" + in.Name}, nil
+}
+
+func main() {
+	ln, err := net.Listen("tcp", post)
+	if err != nil {
+		fmt.Println("网络异常", err)
+	}
+
+	//创建一个grpc句柄
+
+	srv := grpc.NewServer()
+
+	pt.RegisterHelloWorldServiceServer(srv, &server{})
+
+	err = srv.Serve(ln)
+	if err != nil {
+		fmt.Println("网络启动异常", err)
+	}
+}
